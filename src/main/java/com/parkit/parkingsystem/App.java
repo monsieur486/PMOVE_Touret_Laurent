36c826c9dd86cef938e6d05c433f739014322dc8
monsieur486 <monsieur486@gmail.com>
2021-01-13 17:04:09 +0100

    Adding javaDoc comments

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -4,6 +4,9 @@ import com.parkit.parkingsystem.service.InteractiveShell;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Application entry point.
+ */
 public class App {
   private static final Logger logger = LogManager.getLogger("App");
 
