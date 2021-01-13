44d0551059d7f3414868f11127c7d89c7168e72e
monsieur486 <monsieur486@gmail.com>
2021-01-13 18:22:47 +0100

    Adding javaDoc comments

--- src/test/java/com/parkit/parkingsystem/integration/service/DataBasePrepareService.java
+++ src/test/java/com/parkit/parkingsystem/integration/service/DataBasePrepareService.java
@@ -1,13 +1,18 @@
 package com.parkit.parkingsystem.integration.service;
 
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
-
 import java.sql.Connection;
 
+/**
+ * The type Data base prepare service.
+ */
 public class DataBasePrepareService {
 
   DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
 
+  /**
+   * Clear data base entries.
+   */
   public void clearDataBaseEntries() {
     Connection connection = null;
     try {
